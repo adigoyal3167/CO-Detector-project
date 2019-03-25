7dcb026ea0d263be7593f415fb09a82e8911d6bf
adigoyal3167 <48688616+adigoyal3167@users.noreply.github.com>
2019-03-25 12:55:04 -0400

    Add files via upload

--- uart.cpp
+++ uart.cpp
@@ -3,14 +3,15 @@
 #include<unistd.h>
 #include<termios.h>
 #include<string.h>
+#include<wiringPi.h>
 
 //int msg(char *str)
 int main()
 {
-   int count,file;
+   int file, count;
    char buf[10];
  char msg_cmd1[] = "AT+CSCA=\"15149931123\"";
-char msg_cmd2[] = "AT+CMGS=\"12899800419\""; 
+char msg_cmd2[] = "AT+CMGS=\"14377796894\""; 
   if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY))<0){
       perror("UART: Failed to open the device.\n");
       return -1;
@@ -21,7 +22,7 @@ char msg_cmd2[] = "AT+CMGS=\"12899800419\"";
    options.c_iflag = IGNPAR | ICRNL;
    tcflush(file, TCIFLUSH);
    tcsetattr(file, TCSANOW, &options);
-   if ((count = write(file,msg_cmd1,strlen(msg_cmd1))<0){
+   if (count = write(file,msg_cmd1,strlen(msg_cmd1))<0){
       perror("UART: Failed to write to the output\n");
       return -1;
    }
@@ -29,8 +30,8 @@ char msg_cmd2[] = "AT+CMGS=\"12899800419\"";
 
 //read(file,buf,10);
 
-   fprintf(file,"%s\n",msg_cmd1);
-   fscanf(file,"%s",buf);
+  // fprintf(file,"%s\n",msg_cmd1);
+//   fscanf(file,"%s",buf);
 
 printf("here %s ",buf);
 //fflush(stdout);
@@ -41,7 +42,7 @@ if ((count = write(file,msg_cmd2,sizeof(msg_cmd2)))<0){
    }
    write(file, "\n\r", 2);
 
-if ((count = write(file,"Snehal how are you",20))<0){
+if ((count = write(file,"how are you",20))<0){
       perror("UART: Failed to write to the output\n");
       return -1;
    }
