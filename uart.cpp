124ee4563c81ed4dfadafe63e77b36877a368f50
adigoyal3167 <48688616+adigoyal3167@users.noreply.github.com>
2019-03-30 14:29:19 -0400

    Add files via upload

--- uart.cpp
+++ uart.cpp
@@ -5,14 +5,17 @@
 #include<string.h>
 #include<wiringPi.h>
 
-//int msg(char *str)
+	void my_delay_func(long DELAY_TIME)
+{
+for (long jj=0; jj <= DELAY_TIME; jj++);
+}
 int main()
 {
    int file, count;
    char buf[10];
- char msg_cmd1[] = "AT+CSCA=\"15149931123\"";
-char msg_cmd2[] = "AT+CMGS=\"14377796894\""; 
-  if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY))<0){
+   char msg_cmd1[] = "AT+CSCA=\"15149931123\"";
+   char msg_cmd2[] = "AT+CMGS=\"14377796894\""; 
+   if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY))<0){
       perror("UART: Failed to open the device.\n");
       return -1;
    }
@@ -22,26 +25,32 @@ char msg_cmd2[] = "AT+CMGS=\"14377796894\"";
    options.c_iflag = IGNPAR | ICRNL;
    tcflush(file, TCIFLUSH);
    tcsetattr(file, TCSANOW, &options);
+
+   // send msg cmd1 to gsm module (open message centre)
    if (count = write(file,msg_cmd1,strlen(msg_cmd1))<0){
       perror("UART: Failed to write to the output\n");
       return -1;
    }
    write(file, "\n\r", 2);
-
-//read(file,buf,10);
+my_delay_func(2000000);
+   //read(file,buf,10);
 
   // fprintf(file,"%s\n",msg_cmd1);
-//   fscanf(file,"%s",buf);
+  //   fscanf(file,"%s",buf);
 
-printf("here %s ",buf);
-//fflush(stdout);
+  //printf("here %s ",buf);
+  //fflush(stdout);
+  
 
+ // send msg cmd2 to gsm module (specifies recipient)
 if ((count = write(file,msg_cmd2,sizeof(msg_cmd2)))<0){
       perror("UART: Failed to write to the output\n");
       return -1;
    }
    write(file, "\n\r", 2);
+ my_delay_func(2000000);
 
+ // send the actual msg 
 if ((count = write(file,"how are you",20))<0){
       perror("UART: Failed to write to the output\n");
       return -1;
